a3184bd326978256f6ab3e84cfa6cc22db7c174e
Wonho ha <haho013039@gmail.com>
2018-12-18 12:30:19 +0900

    간단한 테스트 작성
    math.js라이브러리를 이용한 수치계산 테스트 추가

--- test/unit_test.js
+++ test/unit_test.js
@@ -1,7 +1,34 @@
 const assert = require('assert');
+const math = require('mathjs');
 describe('Unit Test', function() {
     it('Test 1',function(){
         assert.equal(1,1);
+        assert.equal(0,"0");
+        assert.equal(0,"");
         assert.notEqual(10,-2);
+    });
+    it('Test 2',function(){
+        assert.deepEqual(1,1);
+        assert.notDeepStrictEqual(0,"");
+        assert.notDeepStrictEqual(1,"1");
+    });
+    it('Test 3',function() {
+        const result = math.add(2,3);
+        assert.equal(result, 5);
+    });
+    it('Test 4',function(){
+        const result = math.square(3);
+        assert.equal(result,9);
+    });
+    it('Test 5',function(){
+        const scope = {a:3, b:4};
+        const result = math.eval('a * b', scope);
+        assert.equal(result,3*4)
+    });
+    it('Test 6',function(){
+        const scope = {a:3, b:4,c:23};
+        const result = math.eval('(a * b) * (a - b) * (b * a) * a - (sqrt(c)*sqrt(a))', scope);
+        const right = (3 * 4) * (3 - 4) * (4 * 3) * 3 - (math.sqrt(23)*math.sqrt(3));
+        assert.equal(result, right);
     });
 });
