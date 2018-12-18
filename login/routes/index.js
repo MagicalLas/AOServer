fb613a24441395fcf4638d041f76a1037a465de9
Goto-Namj <nam1jm@naver.com>
2018-12-18 17:32:30 +0900

    중간 저장

--- login/routes/index.js
+++ login/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'ika.today',
+  port     : 4000,
+  user     : 'root',
+  password : 'jin123',
+  database : 'Heroes'
+});
+
+connection.connect();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'Express'
+  });
 });
+
+
 
 module.exports = router;
