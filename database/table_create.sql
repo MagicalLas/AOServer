581ae92b9cdf566399bb34141272af8e1e684979
Wonho ha <haho013039@gmail.com>
2018-12-18 15:25:57 +0900

    Games테이블 구조 추가

--- database/table_create.sql
+++ database/table_create.sql
@@ -19,3 +19,28 @@ CREATE table Trys(
     FOREIGN KEY(userid)
     REFERENCES Users(id) ON UPDATE CASCADE
 );
+
+CREATE table Games(
+    gameid varchar(50) not null,
+    start_time DATETIME not null,
+    end_time DATETIME not null,
+    try1 varchar(50) not null,
+    try2 varchar(50) not null,
+    try3 varchar(50) not null,
+    try4 varchar(50) not null,
+    try5 varchar(50) not null,
+    try6 varchar(50) not null,
+    primary key(gameid),
+        FOREIGN KEY(try1)
+    REFERENCES Trys(id) ON UPDATE CASCADE,
+        FOREIGN KEY(try2)
+    REFERENCES Trys(id) ON UPDATE CASCADE,
+        FOREIGN KEY(try3)
+    REFERENCES Trys(id) ON UPDATE CASCADE,
+        FOREIGN KEY(try4)
+    REFERENCES Trys(id) ON UPDATE CASCADE,
+        FOREIGN KEY(try5)
+    REFERENCES Trys(id) ON UPDATE CASCADE,
+        FOREIGN KEY(try6)
+    REFERENCES Trys(id) ON UPDATE CASCADE
+);
