0f605b944d58af45275bd97e7d09cc3a9b2cab16
Wonho ha <haho013039@gmail.com>
2018-12-18 14:51:45 +0900

    User테이블 수정

--- database/table_create.sql
+++ database/table_create.sql
@@ -1,6 +1,9 @@
-CREATE table user(
+CREATE database Heroes;
+USE Heroes;
+CREATE table Users(
     id varchar(20) not null,
     name varchar(10) not null,
     password varchar(50) not null,
+    point int,
     primary key(id)
 );
