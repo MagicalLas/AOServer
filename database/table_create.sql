1a1ebc64f518df3967825c3593662473965c2f1c
Wonho ha <haho013039@gmail.com>
2018-12-18 15:19:05 +0900

    Trys테이블 추가

--- database/table_create.sql
+++ database/table_create.sql
@@ -7,3 +7,15 @@ CREATE table Users(
     point int,
     primary key(id)
 );
+
+CREATE table Trys(
+    id varchar(50) not null,
+    userid varchar(20) not null,
+    champ varchar(15) not null,
+    win int not null,
+    team varchar(5) not null,
+    game_id varchar(50) not null,
+    primary key(id),
+    FOREIGN KEY(userid)
+    REFERENCES Users(id) ON UPDATE CASCADE
+);
