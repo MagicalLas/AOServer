24d0b3a63d7ea27aa8ec38869517e6f26501db0f
Wonho ha <haho013039@gmail.com>
2018-12-20 20:41:15 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -32,9 +32,11 @@ server.on('connection', (s) => {
     const cc = userCount;
     s.on('data', (data) => {
         const list = data.toString().split('#');
-        console.log(list.length-1);
         for (let ii = 0; ii < list.length-1; ii++) {
+            
+            
             const json = list[ii];
+            console.log(json);
             try {
                 if (json.id == 0) {
                     const jsondata = JSON.stringify(json);
