37d78a0210d790c01ccb5cc91b9bf9649bd48229
Goto-Namj <nam1jm@naver.com>
2018-12-20 17:47:15 +0900

    최종 완성 하였읍니따
    물론 로그인 완료시에 '로그인 성공!'이라는 로그가 뜹니다. 추후에 바꿀 예정.

--- login/routes/index.js
+++ login/routes/index.js
@@ -1,13 +1,61 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+const connection = mysql.createConnection({
+    host: 'ika.today',
+    port: '4000',
+    user: 'root',
+    password: 'jin123',
+    database: 'Heroes',
 });
 
-router.get('../views/register', function(req, res, next) {
-    res.render('register');
+connection.connect();
+
+router.get('/', function(req,res){
+    res.render('basic.pug');
+});
+router.get('/login', function(req,res){
+    res.render('login.pug');
+});
+router.get('/register', function(req,res){
+    res.render('register.pug');
+});
+
+router.post('/login', async function(req,res){
+    const body = req.body;
+    const login_id = body.login_id;
+    const login_password = body.login_password;
+
+    await new Promise((a,b)=>{
+        connection.query(`select * from Users where id='${login_id}'`,(err,row)=>{
+            if( row.length === 0 )
+                console.log('없는 아이디 입니다.');
+            else if( row[0].password === login_password )
+                console.log('로그인 성공!');
+            else if( row[0].password !== login_password)
+                console.log('비밀번호가 올바르지 않습니다.');
+            a();
+        });
+    });
+});
+
+router.post('/register', async function(req,res) {
+    const body = req.body;
+    const register_id = body.register_id;
+    const register_name = body.register_name;
+    const register_password = body.register_password;
+    const point = 0;
+    const sql = `insert into Users values('${register_id}','${register_name}','${register_password}','${point}')`;
+
+    await new Promise((a, b) => {
+        connection.query(sql, (err) => {
+            if (err) {
+            }
+            a();
+        });
+    });
 });
 
 module.exports = router;
