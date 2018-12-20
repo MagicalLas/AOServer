284a60cb92203177e4b92bbe764d992604666e35
Wonho ha <haho013039@gmail.com>
2018-12-20 18:10:42 +0900

    Update main.js

--- server/main.js
+++ server/main.js
@@ -38,7 +38,7 @@ server.on('connection', (s) => {
         if (json.id == 1) {
             const user = hash.makeHash(json.user_id);
             const jsondata = JSON.stringify({ user_id: user, x: json.x, y: json.y, z: json.z, type: json.type });
-            const result = JSON.stringify({ msg: jsondata });
+            const result = JSON.stringify({id:json.id ,msg: jsondata });
             so.Sender(result);
         }
     });
